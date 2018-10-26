d4db3c475dc78768be7a776f9a512f342281e118
dlim <7742500+davejlim@users.noreply.github.com>
2018-10-26 08:59:21 -0700

    Updating Basic Ruby Exercises

--- Basic Ruby Exercises/user_input.rb
+++ Basic Ruby Exercises/user_input.rb
@@ -84,20 +84,109 @@
 # Exercise 7 - In the previous exercise, you wrote a program to solicit a password. In this exercise, you should modify the program so it also requires a user name. The program should solicit both the user name 
 # and the password, then validate both, and issue a generic error message if one or both are incorrect; the error message should not tell the user which item is incorrect.
 
-USERNAME = "admin"
-PASSWORD = "SecreT"
+# USERNAME = "admin"
+# PASSWORD = "SecreT"
 
-loop do
-  puts ">> Please enter user name:"
-  username = gets.chomp
+# loop do
+#   puts ">> Please enter user name:"
+#   username = gets.chomp
 
-  puts ">> Please enter your password:"
-  password = gets.chomp
+#   puts ">> Please enter your password:"
+#   password = gets.chomp
 
-  break if username == USERNAME && password == PASSWORD 
-  puts "Authorization failed!"
+#   break if username == USERNAME && password == PASSWORD 
+#   puts "Authorization failed!"
+# end
+
+# puts "Welcome!"
+
+# Exercise 8 - Write a program that obtains two Integers from the user, then prints the results of dividing the first by the second. 
+# The second number must not be 0, and both numbers should be validated using this method:
+
+# def valid_number?(number_string)
+#   number_string.to_i.to_s == number_string
+# end
+
+# numerator = nil
+# denominator = nil
+
+# loop do
+#   puts ">> Please enter the numerator:"
+#   numerator = gets.chomp
+
+#   break if valid_number?(numerator)
+
+#   puts ">> Invalid input. Only integers are allowed"  
+# end
+
+# loop do
+#   puts ">> Please enter the denominator:"
+#   denominator = gets.chomp
+
+#   break if valid_number?(denominator) && denominator.to_i > 0
+  
+#   if denominator == "0"
+#     puts ">> Invalid input. A denominator of 0 is not allowed."
+#   else
+#     puts ">> Invalid input. Only integers are allowed"  
+#   end
+# end
+
+# answer = numerator.to_i / denominator.to_i
+  
+# puts ">> #{numerator} / #{denominator} is #{answer}"
+
+# Exercise 9 -   Modify this program so it repeats itself after each input/print iteration, asking for a new number each time through. The program should keep running until the user enters q or Q.
+
+# lines = nil
+
+# loop do
+#   puts "How many output lines do you want? Enter a number >= 3 (Q to quit)"
+#   lines = gets.chomp
+
+#   break if lines == "Q" || lines == "q"
+  
+#   lines = lines.to_i
+  
+#   if lines >= 3
+#     lines.times do
+#       puts "LS is the best!"
+#     end  
+#   else 
+#     puts "That's not enough lines."
+#   end
+  
+# end
+
+# Exercise 10 - Write a program that requests two integers from the user, adds them together, and then displays the result. Furthermore, insist that one of the integers be positive, and one negative; 
+# however, the order in which the two integers are entered does not matter.
+
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
-puts "Welcome!"
+first_int = nil
+second_int = nil
 
-# Exercise 8 - 
+loop do
+  puts ">> Please enter a positive or negative integer:"
+  first_int = gets.chomp.to_i
+
+  puts ">> Please enter a positive or negative integer:"
+  second_int = gets.chomp.to_i
+
+  if first_int > 0 && second_int < 0
+    break
+  elsif first_int < 0 && second_int > 0
+    break
+  elsif valid_number?(first_int) || valid_number?(second_int)
+    puts ">> Invalid input. Only non-zero integers are allowed."
+  end
+end
+
+answer = first_int + second_int
+
+puts "#{first_int} + #{second_int} = #{answer}"
+  
+
+
