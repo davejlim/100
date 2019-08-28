5e23203f1b8181b7fa3036663b26fa1ece06d2a6
dlim <7742500+davejlim@users.noreply.github.com>
2019-08-28 11:47:38 -0700

    Finished the Strings exercises

--- Basic Ruby Exercises/strings.rb
+++ Basic Ruby Exercises/strings.rb
@@ -40,10 +40,94 @@
 # Exercise 5 - Using the following code, combine the two names together to form a full name and assign that value to a variable named full_name. 
 # Then, print the value of full_name.
 
-first_name = 'John'
-last_name = 'Doe'
+# first_name = 'John'
+# last_name = 'Doe'
 
-full_name = "#{first_name} #{last_name}"
-puts full_name
+# full_name = "#{first_name} #{last_name}"
+# puts full_name
 
-# Exercise 6 - 
+# This is one way to concatenate the strings. I used string interpolation. Another way is just using the + to concatenate the strings and including a space - 
+# full_name = first_name + " " + last_name
+# Important to note that concatenation returns a new string rather than mutating it's caller. That means that the new string is stored under full_name but doesn't change
+# the previous variables. 
+
+# Exercise 6 - Using the following code, capitalize the value of state then print the modified value. Note that state should have the modified value both before and after you print it.
+
+# state = 'tExAs'
+
+# puts state
+
+# state.capitalize!
+
+# puts state
+
+# Remember that ! makes the method destructive, which means that it actually modififes and mtuates the caller. So aftewards, state will continue to have the modifed
+# and in this case, the corrected value afterwards.
+
+# Exercise 7 - Given the following code, invoke a destructive method on greeting so that Goodbye! is printed instead of Hello!.
+
+# greeting = 'Hello!'
+
+# puts greeting.replace "Goodbye!"
+# puts greeting
+
+# LS proposes a solution utilizng the gsub method (which passes in 2 arguments). It utilizes the second argument to replace whatever was in the first argument of the string.
+
+# greeting = 'Hello!'
+
+# puts greeting.gsub!("Hello!","Goodbye!")
+# puts greeting
+
+# My solution also is a destructive method that mutates the caller and replaces the greeting string.
+
+# Exercise 8 - Using the following code, split the value of alphabet by individual characters and print each character.
+
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# alphabet.each_char {|c| puts c}
+
+# My solution utilizes the each_char method to pass each character in str to the given block which I then used the puts method to ensure that each character was then 
+# printed on its own line like the exercise had requested for the output.
+
+# The LS solution proposes using the split method which creates an array that splits the string based on the character that you pass in as an argument.
+
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# puts alphabet.split("")
+
+# If you pass in an empty string into the split method as an argument, Ruby will split the string into an array of characters. 
+
+# Exercise 9 - Given the following code, use Array#each to print the plural of each word in words.
+
+# words = 'car human elephant airplane'
+
+# plural = words.split(" ")
+# puts plural.each {|w| w.concat("s")}
+
+# LS solution proposes just appending with +
+
+# words = 'car human elephant airplane'
+
+# words.split(" ").each do |word|
+#    puts word + "s"
+# end
+
+# Exercise 10 - Using the following code, print true if colors includes the color 'yellow' and print false if it doesn't. Then, print true if colors includes the color 'purple' and print false if it doesn't.
+
+# colors = 'blue pink yellow orange'
+
+# puts colors.include?("yellow")
+# puts colors.include?("purple")
+
+# Important to note that include is case-sensitive. In Ruby, comparison of strings is case-sensitive. The include method is checking if the string includes the argument being passed in. So
+# if you passed in "ink", it would still be true, because it is contained within the color pink.
+
+
+
+
+
+
+
+
+
+
+
